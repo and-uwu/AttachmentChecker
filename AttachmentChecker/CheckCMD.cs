dcf3132f4ea4e53046e1052f532dd1345028af37
not-gm1 <prodima1847@gmail.com>
2025-01-25 23:33:45 +0300

    Добавьте файлы проекта.

---
+++ AttachmentChecker/CheckCMD.cs
@@ -0,0 +1,49 @@
+﻿using CommandSystem;
+using Exiled.API.Features;
+using Exiled.API.Features.Items;
+using Exiled.API.Structs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttachmentChecker
+{
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    public class CheckCMD : ICommand
+    {
+        public string Command => "checkattachment";
+
+        public string[] Aliases { get; } = {"checkatt", "chkatt"};
+
+        public string Description => "Check attachments on weapon";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            if(!sender.CheckPermission(PlayerPermissions.PlayersManagement))
+            {
+                response = "Permission denied";
+                return false;
+            }
+            if (!Player.TryGet(sender, out Player player))
+            {
+                response = "You are not player";
+                return false;
+            }
+            if (player.CurrentItem is not Firearm firearm)
+            {
+                response = "You don't have firearm";
+                return false;
+            }
+            StringBuilder stb = new();
+            stb.Append("Attachments:\n");
+            foreach (AttachmentIdentifier attachment in firearm.AttachmentIdentifiers)
+            {
+                stb.Append($"- {attachment.Name.ToString()}\n");
+            }
+            response = stb.ToString();
+            return true;
+        }
+    }
+}
---
+++ AttachmentChecker/Plugin.cs
@@ -0,0 +1,31 @@
+﻿using Exiled.API.Features;
+using Exiled.API.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttachmentChecker
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = false;
+    }
+
+    public class Plugin: Plugin<Config>
+    {
+        public override string Author => "and.you";
+        public override string Name => "AttachmentChecker";
+        public override string Prefix => "AttachmentChecker";
+        public override void OnEnabled()
+        {
+            base.OnEnabled();
+        }
+        public override void OnDisabled()
+        {
+            base.OnDisabled();
+        }
+    }
+}
